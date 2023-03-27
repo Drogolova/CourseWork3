07963b57fccab06cfb44edcf4dbed90fe9e9580e
Drogolova <drogolova1997@mail.ru>
2023-03-27 18:48:27 +0300

    Курсовая работа 3

---
+++ src/test/java/socks/shop/coursework3/CourseWork3ApplicationTests.java
@@ -0,0 +1,13 @@
+package socks.shop.coursework3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
